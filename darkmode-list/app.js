c7ea83dfb2c8b7daf40b07d83a38badafe319638
johnsmillie <jsmillie23@gmail.com>
2024-09-17 22:16:11 -0500

    week 1 playground

--- darkmode-list/app.js
+++ darkmode-list/app.js
@@ -1,14 +1,14 @@
 //must code in a certain way
 'use strict'
 
-const switcher = document.querySelector('.btn');
+const switcher = document.querySelector('.mybtn');
 
 switcher.addEventListener('click', function(){
-    document.body.classList.toggle('lightTheme');
-    document.body.classList.toggle('darkTheme');
+    document.body.classList.toggle('lightTheme1');
+    document.body.classList.toggle('darkTheme1');
 
     const className = document.body.className;
-    if(className == 'lightTheme'){
+    if(className == 'lightTheme1'){
         this.textContent = "Dark";
     }
     else{
@@ -16,4 +16,11 @@ switcher.addEventListener('click', function(){
     }
 });
 
+/*const myModal = document.getElementById('exampleModal');
+const myInput = document.getElementById('exampleModalLabel');
 
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+*/
