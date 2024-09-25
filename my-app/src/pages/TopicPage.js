f5b3abbe719f4a2dbba14e01b6edc908bb50a538
johnsmillie <jsmillie23@gmail.com>
2024-09-25 15:46:11 -0500

    css adjustments

--- my-app/src/pages/TopicPage.js
+++ my-app/src/pages/TopicPage.js
@@ -4,7 +4,7 @@ import "../css/TopicPage.css";
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import ResponsePara from "../components/ResponsePara";
-import { Container,Button } from "react-bootstrap";
+import { Container,Button,Badge } from "react-bootstrap";
 import posts from "../data/posts.json";
 import comments from "../data/comments.json";
 import ResponseCreatorButton from "../components/ResponseCreatorButton";
@@ -16,7 +16,7 @@ function TopicPage(){
     <br></br>
       {posts.map((post, index) => (
         <div>
-          <Card style={{ width: '85rem' }}>
+          <Card style={{ width: '100%' }}>
              <Card.Body>
                <Card.Title>{post.title}</Card.Title>
                <Card.Subtitle className="mb-2 text-muted">OP: {post.id}</Card.Subtitle>
@@ -28,17 +28,19 @@ function TopicPage(){
                {' '} {post.rating}{' '}
                <Button><ArrowDown></ArrowDown></Button>{' '}
                <ResponseCreatorButton/>
-               
+             
                <Accordion>
                   <Accordion.Item eventKey="0">
-                    <Accordion.Header>Comments</Accordion.Header>
+                    <Accordion.Header ><span class="badgeMargin" >Comments</span>
+                    <Badge bg="info">{comments.filter(comment => comment.postID === post.id).length}</Badge>    </Accordion.Header>
                       <Accordion.Body>
+                       
                       <ResponsePara comments={comments.filter(comment => comment.postID === post.id)  }> 
                       </ResponsePara>
                       </Accordion.Body>
                   </Accordion.Item>
                </Accordion>
-               
+             
              </Card.Body>
          
           </Card>
